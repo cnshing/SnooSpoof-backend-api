3e0324ce353426ba50ca5f7c5129404d999eede3
Shing Chan <chan.shing@protonmail.com>
2022-10-01 10:48:00 +0000

    Add more documentation and type alias to scrapper.py

--- generate/scrapper.py
+++ generate/scrapper.py
@@ -11,11 +11,27 @@ class PRAWExtension():
     """
 
     def __init__(self, reddit: praw.Reddit):
+        """Plug in the a reddit instance to allow extended
+        functionality
+
+        Args:
+            reddit (praw.Reddit): A PRAW Reddit Instance
+        """
         self.reddit = reddit
 
-    def _get(self, listing, attr: Iterable):
+    def _get(self,
+            listing,
+            attr: Iterable) -> dict[str, ]:
         """
-        Get a a listing's attributes as a dictionary.
+        Get a listing's attributes as a dictionary.
+
+        Args:
+            listing (obj): Any object with attributes.
+            attr (Iterable): An iterable of attributes.
+
+        Returns:
+            dict[str, ]: An attribute keyed by their attribute name
+            and valued by the attribute's value
         """
         values = attrgetter(*attr)(listing)
         return dict(zip(attr, values))
@@ -23,7 +39,7 @@ class PRAWExtension():
     def _search(self,
                 request: praw.models.ListingGenerator,
                 attr: Iterable,
-                filter_fn: Callable[[], bool]=lambda default: True):
+                filter_fn: Callable[[], bool]=lambda default: True) -> list[dict[str, ]]:
         """Queries a listing for certain attributes. Elements of the listing that do not
         pass filter will not be saved.
 
@@ -32,18 +48,33 @@ class PRAWExtension():
             https://praw.readthedocs.io/en/latest/search.html?q=ListingGenerator
             attr (_type_): A iterable of attributes
             filter (func): Boolean function with an element of the listing as the single parameter
+
+        Returns:
+            list[dict[str, ]]: A list of elements with keys as attributes and
+            values as the attribute's values
         """
         return [self._get(listing, attr) for listing in request if filter_fn(listing)]
 
     def _check_author_ids(self,
                         author: str = None,
-                        ids: Iterable[str] = None):
+                        ids: Iterable[str] = None) -> None:
+        """Prevents situations where an indecision is made between
+        searching for authors or searching for ids
+
+        Args:
+            author (str, optional): A redditor's username. Defaults to None.
+            ids (Iterable[str], optional): An iterable of ids. Defaults to None.
+
+        Raises:
+            ValueError: Neither author and ids are defined
+            ValueError: Both authors and ids are defined
+        """
         if author == ids == None:
             raise ValueError('Either an reddit username or ids be specified')
         elif author != ids != None:
             raise ValueError('Cannot search for both authors and ids at the same time')
 
-    def is_comment(self, fullname: str):
+    def is_comment(self, fullname: str) -> bool:
         """Check to see if a fullname is a comment
 
         Args:
@@ -54,7 +85,7 @@ class PRAWExtension():
         """
         return fullname.startswith("t1_")
 
-    def is_submission(self, fullname: str):
+    def is_submission(self, fullname: str) -> bool:
         """Check to see if a fullname is a submission
 
         Args:
@@ -71,7 +102,7 @@ class PRAWExtension():
                         author: str = None,
                         ids: Iterable[str] = None,
                         filter_fn: Callable[[], bool]=lambda default: True,
-                        **kawrgs):
+                        **kawrgs) -> list[dict[str, ]]:
         """Searches for submissions by IDs or author, returning the metadata in tags.
         If a filter_fn is specified, only submissions that pass the filter are saved.
 
@@ -83,7 +114,7 @@ class PRAWExtension():
             filter (func): Boolean function with an element of the listing as the single parameter
 
         Returns:
-            List[dict]: A list of submissions
+            list[dict[str, ]]: A list of submissions
         """
         self._check_author_ids(author, ids)
 
@@ -102,7 +133,7 @@ class PRAWExtension():
                     author: str = None,
                     ids: Iterable[str] = None,
                     filter_fn: Callable[[], bool]=lambda default: True,
-                    **kawrgs):
+                    **kawrgs) -> list[dict[str, ]]:
         """Searches for comments by IDs or author. returning the metadata in tags.
         If a filter_fn is specified, only comments that pass the filter are saved.
 
@@ -114,7 +145,7 @@ class PRAWExtension():
             filter (func): Boolean function with an element of the listing as the single parameter
 
         Returns:
-            List[dict]: A list of comments
+            list[dict[str, ]]: A list of comments
         """
         self._check_author_ids(author, ids)
 
@@ -127,11 +158,14 @@ class PRAWExtension():
             comments = self._search(listing(**kawrgs), attr=tags, filter_fn=filter_fn)
         return comments
 
-    def seperate_posts(self, ids: Iterable[str]):
+    def seperate_posts(self, ids: Iterable[str]) -> tuple[list[str], list[str]]:
         """Seperate a list of ids into comment ids and submission ids
 
         Args:
             ids (Iterable[str]): A list of fullnames, characterized by a t3_ or t_1 prefix
+
+        Returns:
+            tuple[list[str], list[str]]: A tuple of submission ids and comment ids, by that order
         """
         def seperate(restriction):
             return list(filter(restriction, ids))
@@ -141,7 +175,7 @@ class PRAWExtension():
                     author: str = None,
                     parent_ids: Iterable[str] = None,
                     submission_tags: Iterable[str] = None,
-                    comment_tags:  Iterable[str] = None):
+                    comment_tags:  Iterable[str] = None) -> dict[str, dict[str, ]]:
         """Search the parents of a comment by author or ID. When no IDs are given,
         search by the author's comments instead, following the same parameters
         as search_comments().
@@ -153,7 +187,7 @@ class PRAWExtension():
             comment_tags (Iterable[str], optional): Metadata for comments. Defaults to None.
 
         Returns:
-            dict: A dictionary of parents keyed by their unique parent id
+            dict[str, dict[str, ]]: A dictionary of parents keyed by their unique parent id
         """
         self._check_author_ids(author, parent_ids)
 
